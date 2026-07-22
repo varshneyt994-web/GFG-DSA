e5849b26f6acf35808af2a1a879618966be9a734
varshneyt994-web <varshneyt994@gmail.com>
2026-07-22 21:46:33 +0530

    Added solution - Time: O(n log n), Space: O(log n)

---
+++ 701220-majority-element-GFG/701220-majority-element.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+class Solution {
+    int majorityElement(int arr[]) {
+
+        Arrays.sort(arr);
+
+        int count = 1;
+        int ans = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] == arr[i - 1]) {
+                count++;
+            } else {
+                count = 1;
+            }
+
+            if (count > arr.length / 2) {
+                return arr[i];
+            }
+        }
+
+        // Single element case
+        if (arr.length == 1) {
+            return arr[0];
+        }
+
+        return -1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
