27e97d9dec2cd5e85a437c08ec86a9c3ce03dde2
varshneyt994-web <varshneyt994@gmail.com>
2026-07-24 16:28:34 +0530

    Added solution - Time: O(log n), Space: O(1)

---
+++ 878537-implement-lower-bound-GFG/878537-implement-lower-bound.java
@@ -0,0 +1,24 @@
+class Solution {
+    int lowerBound(int[] arr, int target) {
+        int n = arr.length;
+        int lo = 0, hi = n - 1;
+        int lb = n;
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (arr[mid] >= target) {
+                lb = mid;
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+
+        return lb;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
