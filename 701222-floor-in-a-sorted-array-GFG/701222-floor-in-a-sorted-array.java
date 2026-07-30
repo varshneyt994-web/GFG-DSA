ada4b87341621dcecc49c1d7307bfb5bcf409a48
varshneyt994-web <varshneyt994@gmail.com>
2026-07-30 17:36:25 +0530

    Added solution - Time: O(log n), Space: O(1)

---
+++ 701222-floor-in-a-sorted-array-GFG/701222-floor-in-a-sorted-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    static int findFloor(int[] arr, int x) {
+
+        int lo = 0;
+        int hi = arr.length - 1;
+        int ans = -1;
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (arr[mid] <= x) {
+                ans = mid;      
+                lo = mid + 1;   
+            } else {
+                hi = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
