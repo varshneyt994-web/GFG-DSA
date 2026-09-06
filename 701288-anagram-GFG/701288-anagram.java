eebef86c821f5c2735778ca087571eee1a43bef3
varshneyt994-web <varshneyt994@gmail.com>
2026-09-06 09:38:42 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 701288-anagram-GFG/701288-anagram.java
@@ -0,0 +1,40 @@
+class Solution {
+    public static boolean areAnagrams(String s, String t) {
+        // code here
+        HashMap<Character, Integer> smap = new HashMap<>();
+          if(s.length()!=t.length()) return false;
+             for (int i = 0; i < s.length(); i++) {
+             char key = s.charAt(i);
+             if (smap.containsKey(key)) {
+                int freq = smap.get(key);
+                smap.put(key, freq + 1);
+            } else {
+                smap.put(key, 1);
+            }
+        }
+               HashMap<Character, Integer> tmap = new HashMap<>();
+               for (int i = 0; i < t.length(); i++) {
+               char key = t.charAt(i);
+                 if (tmap.containsKey(key)) {
+                int freq = tmap.get(key);
+                tmap.put(key, freq + 1);
+            } else {
+                tmap.put(key, 1);
+            }
+        }
+            for (int i = 0; i < t.length(); i++) {
+            char key = t.charAt(i);
+            if (!smap.containsKey(key) || !smap.get(key).equals(tmap.get(key))) {
+                return false;
+            }
+        }
+
+        return true;
+
+            }
+        
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
