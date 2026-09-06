7106f426663d7eca8e129ba7c19cf0b735be5347
varshneyt994-web <varshneyt994@gmail.com>
2026-09-06 10:55:35 +0530

    Added solution - Time: O(n), Space: O(k)

---
+++ 702991-count-equal-pairs-in-string-GFG/702991-count-equal-pairs-in-string.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int equalPairs(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        int pairs = 0;
+
+        for (Character ch : map.keySet()) {
+            long freq = map.get(ch);
+            pairs += freq * freq;
+        }
+
+        return pairs;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
